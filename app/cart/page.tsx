eeabd1f866aeb3b0a9dddf09d002e61bebf42ffd
Prathyusha Chinthakunta <chinthakuntaprathyusha2001@gmail.com>
2024-04-25 11:03:55 +0530

    update:cart image size is adjusted

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 interface CartProps {
   id: number;
 }
+
 const Cart = ({ id }: CartProps) => {
   const selectedItem = data.find((item) => item.id === id);
 
@@ -23,7 +24,7 @@ const Cart = ({ id }: CartProps) => {
   return (
     <Box bgcolor="black" mb={2} height="90%">
       <Link href={url} underline="none" color="inherit">
-        <Card sx={{ p: 2, height: "100%" }}>
+        <Card sx={{ p: 2, height: "100%", position: "relative" }}>
           {dealPrice !== regularPrice && (
             <Typography
               variant="body1"
@@ -32,6 +33,10 @@ const Cart = ({ id }: CartProps) => {
                 backgroundColor: "green",
                 color: "white",
                 padding: "2px 8px",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                zIndex: 1,
               }}
             >
               {discountPercentage}% off
@@ -68,3 +73,74 @@ const Cart = ({ id }: CartProps) => {
 };
 
 export default Cart;
+
+// import React from "react";
+// import { Box, Card, Typography, Link } from "@mui/material";
+// import data from "../../dummy-data.json";
+// import Image from "next/image";
+
+// interface CartProps {
+//   id: number;
+// }
+// const Cart = ({ id }: CartProps) => {
+//   const selectedItem = data.find((item) => item.id === id);
+
+//   if (!selectedItem) {
+//     return null;
+//   }
+
+//   const { title, mrp, asin, image, url, dealPrice, regularPrice } =
+//     selectedItem;
+//   const truncatedTitle =
+//     title.length > 50 ? `${title.substring(0, 50)}...` : title;
+
+//   const discountPercentage = Math.round(((mrp - dealPrice) / mrp) * 100);
+
+//   return (
+//     <Box bgcolor="black" mb={2} height="90%">
+//       <Link href={url} underline="none" color="inherit">
+//         <Card sx={{ p: 2, height: "100%" }}>
+//           {dealPrice !== regularPrice && (
+//             <Typography
+//               variant="body1"
+//               sx={{
+//                 display: "inline",
+//                 backgroundColor: "green",
+//                 color: "white",
+//                 padding: "2px 8px",
+//               }}
+//             >
+//               {discountPercentage}% off
+//             </Typography>
+//           )}
+//           <Box position="absolute">
+//             <Image src={image} alt={title} width={400} height={400} />
+//           </Box>
+//           <Typography variant="h6">{truncatedTitle}</Typography>
+
+//           <Box display="flex">
+//             <Typography variant="body1">₹{dealPrice} </Typography>
+//             <Typography
+//               variant="body1"
+//               sx={{
+//                 color: "text.disabled",
+//                 marginLeft: 1,
+//                 marginRight: 1,
+//                 textDecoration: "line-through",
+//               }}
+//             >
+//               ₹{mrp}
+//             </Typography>
+//             <Typography variant="body1" sx={{ color: "green" }}>
+//               {discountPercentage}% off
+//             </Typography>
+//           </Box>
+//           <Typography variant="body1">Regular Price: {regularPrice}</Typography>
+//           <Typography variant="body1">ASIN: {asin}</Typography>
+//         </Card>
+//       </Link>
+//     </Box>
+//   );
+// };
+
+// export default Cart;
